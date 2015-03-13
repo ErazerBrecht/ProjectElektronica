8886e175066c666eee490bf311cb46d55a2ad900
ErazerBrecht <brecht.carlier@student.ap.be>
2015-03-13 21:25:57 +0100

    Fixed these files I accidently corrupted these files Still a noob in git...

--- RobotArduino/Ultrasoon.cpp
+++ RobotArduino/Ultrasoon.cpp
@@ -8,79 +8,44 @@ Created by Brecht Carlier & Arne Schoonvliet
 #include "Ultrasoon.h"
 
 //Constructor
-<<<<<<< HEAD:Sensor/Ultrasoon.cpp
-//We have two constructors
-//One accepts 2 pins (one sensor)
-//Another accepts 4 pins (two sensor)
-//The Library is made with native support to fuse two sensors. As example: you could use two forward sensors for big robots. Arduino will see it as one!
-//Conclusion: The Arduino program will not change!!!
-//TODO: Filter init in seperate method
-Ultrasoon::Ultrasoon(int trigPin, int echoPin)
-=======
 void Ultrasoon::begin(int trigPin, int echoPin)
->>>>>>> NewOO:RobotArduino/Ultrasoon.cpp
 {
-	_fusion = false;
-
 	_trigPin = trigPin;		//Save the constuctor parameter (pin) to a private field (is used later for measuring)
 	_echoPin = echoPin;
-
-	Filter.begin();			//Start the filter library
-	Filter.setFilter('m');  //Set it's mode on "median" 
-	Filter.setOrder(3);		//Set the number of sample the filter uses to calculate the median!
-
-	pinMode(_trigPin, OUTPUT);
-	pinMode(_echoPin, INPUT);
-}
-
-Ultrasoon::Ultrasoon(int trigPin, int echoPin, int trigPin2, int echoPin2)
-{
-	_fusion = true;
-
-	_trigPin = trigPin;		//Save the constuctor parameter (pin) to a private field (is used later for measuring)
-	_echoPin = echoPin;
-	_trigPin2 = trigPin2;		
-	_echoPin2 = echoPin2;
 
 	//TODO: Make Filter init function!
-	Filter.begin();			//Start the filter library
-	Filter.setFilter('m');  //Set it's mode on "median" 
-	Filter.setOrder(3);		//Set the number of sample the filter uses to calculate the median!
+	//Filter.begin();			//Start the filter library
+	//Filter.setFilter('m');  //Set it's mode on "median" 
+	//Filter.setOrder(3);		//Set the number of sample the filter uses to calculate the median!
 
 	pinMode(_trigPin, OUTPUT);
 	pinMode(_echoPin, INPUT);
-	pinMode(_trigPin2, OUTPUT);
-	pinMode(_echoPin2, INPUT);
 }
 
 //Private!
 //The main function of this library
-<<<<<<< HEAD:Sensor/Ultrasoon.cpp
-//This function will do the actual measuring!
-long Ultrasoon::calculateCentimeter(int trigPin, int echoPin)
-=======
 //This function will do the measuring!
 void Ultrasoon::getCentimeter()
->>>>>>> NewOO:RobotArduino/Ultrasoon.cpp
 {
 	// The sensor is triggered by a HIGH pulse of 10 or more microseconds.
 	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-	digitalWrite(trigPin, LOW);
+	digitalWrite(_trigPin, LOW);
 	delayMicroseconds(2);
 
-	digitalWrite(trigPin, HIGH);
+	digitalWrite(_trigPin, HIGH);
 	delayMicroseconds(10);
 
-	digitalWrite(trigPin, LOW);
-	_duration = pulseIn(echoPin, HIGH);
+	digitalWrite(_trigPin, LOW);
+	_duration = pulseIn(_echoPin, HIGH);
 
 	//Calculate the distance (in cm) and filter against fault readings!
-	_filteredDistance = Filter.run(_duration / 58.2);
+	//_filteredDistance = Filter.run(_duration / 58.2);
+	_filteredDistance = _duration / 58.2;
 
 	//Check if sensor value is out of range!
-	if (_filteredDistance < 3)
+	if (_filteredDistance < 2)
 	{
-		_filteredDistance = 2;
+		_filteredDistance = 1;
 	}
 
 	//Check if sensor value is out of range!
@@ -88,32 +53,6 @@ void Ultrasoon::getCentimeter()
 	{
 		_filteredDistance = 401;
 	}
-
-	return _filteredDistance;
-
-}
-
-//Two private functions that add the stability delay
-//With the fusion method we will return the smallest value.
-//This value is closer to wall and is thus more important! 
-void Ultrasoon::getCentimeter()
-{
-	_centimeter = calculateCentimeter(_trigPin, _echoPin);
-	delay(25);
-<<<<<<< HEAD:Sensor/Ultrasoon.cpp
-}
-
-void Ultrasoon::getCentimeterFusion()
-{
-	_centimeter = calculateCentimeter(_trigPin, _echoPin);
-	delay(25);
-	_centimeter2 = calculateCentimeter(_trigPin2, _echoPin2);
-	delay(25);
-
-	if (_centimeter2 < _centimeter)
-		_centimeter = _centimeter2;
-=======
->>>>>>> NewOO:RobotArduino/Ultrasoon.cpp
 }
 
 //First measure distance with private function (getCentimeter)
@@ -121,12 +60,8 @@ void Ultrasoon::getCentimeterFusion()
 //If this is the case return true otherwise return false!
 bool Ultrasoon::isCloser(int x)
 {
-	if (_fusion == false)
-		getCentimeter();
-	else
-		getCentimeterFusion();
-
-	if (_centimeter < x)
+	getCentimeter();
+	if (_filteredDistance < x)
 	{
 		return true;
 	}
--- RobotArduino/Ultrasoon.h
+++ RobotArduino/Ultrasoon.h
@@ -13,36 +13,16 @@
 class Ultrasoon
 {
   public:
-<<<<<<< HEAD:Sensor/Ultrasoon.h
-    Ultrasoon(int trigPin, int echoPin); 
-	Ultrasoon(int trigPin, int echoPin, int trigPin2, int echoPin2);
-=======
     void begin(int trigPin, int echoPin); 
->>>>>>> NewOO:RobotArduino/Ultrasoon.h
 	bool isCloser(int x);
 
   private:
     int _trigPin;
 	int _echoPin;
-	int _trigPin2;
-	int _echoPin2;
-
-	bool _fusion;
-    
-	SignalFilter Filter;
-	
+    SignalFilter Filter;
 	long _duration;
 	long _filteredDistance;
-<<<<<<< HEAD:Sensor/Ultrasoon.h
-	long _centimeter;		//TODO: Needs to become an array!
-	long _centimeter2;
-
 	void getCentimeter();
-	void getCentimeterFusion();
-	long calculateCentimeter(int trigPin, int echoPin);
-=======
-	void getCentimeter();
->>>>>>> NewOO:RobotArduino/Ultrasoon.h
 };
 
 #endif
